3424188ca950def3e9d801c9941c24880390c9ef
Utente <liscofrancesco01@gmail.com>
2023-02-21 10:18:33 +0100

    Registrazione Animale Funzionante

--- app/src/main/java/it/uniba/dib/sms22239/Animale.java
+++ app/src/main/java/it/uniba/dib/sms22239/Animale.java
@@ -3,7 +3,10 @@ package it.uniba.dib.sms22239;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Random;
 
 public class Animale
@@ -12,27 +15,27 @@ public class Animale
     public String nome;
     public String razza;
     public String sesso;
-    public SimpleDateFormat data;
+    public String data;
     public String Id_utente;
-    public DatabaseReference mDatabase;
 
     public Animale() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public void writeNewAnimal(Animale ani, String nome, String razza, String sesso, String data, String Id_utente) {
+    public void writeNewAnimal(Animale ani, String nome, String razza, String Id_utente, String sesso, String data)
+    {
         ani.nome = nome;
         ani.razza = razza;
         ani.sesso = sesso;
         ani.Id_utente = Id_utente;
-        ani.data = new SimpleDateFormat(data);
+        ani.data = data;
 
         long seed = System.currentTimeMillis(); // ottenere il tempo corrente
         Random random = new Random(seed); // creare un oggetto Random con il tempo come seme
         int tmp = Math.abs(random.nextInt()); // generare un numero casuale
-        ani.Id = Integer.toString(tmp);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("Animale").child(Id).setValue(ani);
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("Animale").child(Integer.toString(tmp)).setValue(ani);
+        ani.Id = Integer.toString(tmp);
     }
 }
--- app/src/main/java/it/uniba/dib/sms22239/RegistrazioneAnimale0.java
+++ app/src/main/java/it/uniba/dib/sms22239/RegistrazioneAnimale0.java
@@ -15,12 +15,15 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.text.CharacterIterator;
 import java.text.ParseException;
+import java.text.StringCharacterIterator;
 
 
 public class RegistrazioneAnimale0 extends AppCompatActivity {
     EditText inputNome, inputRazza, inputData;
     RadioGroup inputSesso;
+    String sesso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +33,21 @@ public class RegistrazioneAnimale0 extends AppCompatActivity {
         inputNome=findViewById(R.id.register_animal_name);
         inputRazza=findViewById(R.id.register_animal_species);
         inputData=findViewById(R.id.register_animal_birthdate);
-        RadioGroup inputSesso = (RadioGroup) findViewById(R.id.register_animal_sex);
-        final String[] sesso = new String[1];
+        inputSesso = (RadioGroup) findViewById(R.id.register_animal_sex);
+        ;
 
         inputSesso.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // Get the selected radio button from the group
                 RadioButton radioButton1 = (RadioButton) group.findViewById(R.id.register_animal_male);
                 if (radioButton1 != null) {
-                    sesso[0] = "Maschio";
+                    sesso = "Maschio";
                 }
                 RadioButton radioButton2 = (RadioButton) group.findViewById(R.id.register_animal_female);
                 if (radioButton2 != null) {
-                    sesso[0] = "Femmina";
+                    sesso = "Femmina";
                 }
             }
         });
@@ -57,8 +61,21 @@ public class RegistrazioneAnimale0 extends AppCompatActivity {
                 Animale ani = new Animale();
                 String nome = inputNome.getText().toString();
                 String razza = inputRazza.getText().toString();
-                String data = inputData.getText().toString();
-                ani.writeNewAnimal(ani, nome, razza, sesso[0], data, currentUser.getUid());
+                String datatmp = inputData.getText().toString();
+                String data = "";
+                CharacterIterator it = new StringCharacterIterator(datatmp);
+
+                while (it.current() != CharacterIterator.DONE)
+                {
+                    if(it.getIndex() == 4 || it.getIndex() == 6 )
+                    {
+                        data = data + "/";
+                    }
+                    data = data + it.current();
+                    it.next();
+                }
+
+                ani.writeNewAnimal(ani, nome, razza, currentUser.getUid(), sesso, data);
                 Intent intent = new Intent(RegistrazioneAnimale0.this, QRGenerate.class);
                 intent.putExtra("ANIMAL_NAME", "Leone"); // sostituisci con il nome dell'animale dell'utente
                 startActivity(intent);
