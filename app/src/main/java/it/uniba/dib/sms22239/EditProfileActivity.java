5ef55318e88d5b64bdcb3a7b81fdb429d7707500
Fede <giuliani.f2001@gmail.com>
2023-02-21 11:13:19 +0100

    sezione modifica profilo e visualizza profilo funzionante, possibilitá di modificare nome e cognome

--- app/src/main/java/it/uniba/dib/sms22239/EditProfileActivity.java
+++ app/src/main/java/it/uniba/dib/sms22239/EditProfileActivity.java
@@ -1,5 +1,6 @@
 package it.uniba.dib.sms22239;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -10,52 +11,89 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class EditProfileActivity extends AppCompatActivity {
 
     private EditText editName;
-    private EditText editEmail;
+
+    private EditText editCognome;
+
+    private TextView mNomeTextView;
+    private TextView mCognomeTextView;
+
     private Button saveProfileButton;
 
+    private FirebaseAuth mAuth;
+
+    private FirebaseDatabase userDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        // Collega i componenti dell'interfaccia utente con le variabili
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        DatabaseReference mDatabase;
+
+        mDatabase = database.getInstance().getReference().child("User").child(user.getUid());
+
+        // Collega i componenti dell'interfaccia con le variabili
         editName = findViewById(R.id.edit_name);
-        editEmail = findViewById(R.id.edit_email);
+        editCognome = findViewById(R.id.edit_cognome);
+
+
         saveProfileButton = findViewById(R.id.save_profile_button);
 
+        mNomeTextView = findViewById(R.id.edit_name);
+        mCognomeTextView = findViewById(R.id.edit_cognome);
 
-        // Imposta un listener di clic sul pulsante di salvataggio del profilo
+        // Recupera i dati dal database e popola i campi
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //recupero dati e assegnazione alle variabili
+                String name = dataSnapshot.child("nome").getValue(String.class);
+                String cognome = dataSnapshot.child("cognome").getValue(String.class);
+
+                //set delle variabili recuperate al layout
+                mNomeTextView.setText(name);
+                mCognomeTextView.setText(cognome);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+             });
+
+            // Imposta un listener di clic sul pulsante di salvataggio del profilo
         saveProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 // Salva i dati del profilo e torna all'activity precedente
+                DatabaseReference mDatabase = database.getInstance().getReference().child("User").child(user.getUid());
+
+                //prende i dati inseriti in input e gli assegna alle variabili temporanee
                 String newName = editName.getText().toString();
-                String newEmail = editEmail.getText().toString();
+                String newCognome = editCognome.getText().toString();
 
-                // Creazione del Bundle per passare la nuova email a UserProfileActivity
-                Bundle bundle = new Bundle();
-                bundle.putString("newEmail", newEmail);
+                //modifica e salva i dati anche sul database
+                mDatabase.child("cognome").setValue(newCognome);
+                mDatabase.child("nome").setValue(newName);
 
-                // Impostazione del risultato dell'Activity e chiusura dell'Activity
-                Intent resultIntent = new Intent();
-                resultIntent.putExtras(bundle);
-                setResult(Activity.RESULT_OK, resultIntent);
+                //intent per tornare alla schermata del profilo
                 Intent intent = new Intent(EditProfileActivity.this, Profile_Activity.class);
-                intent.putExtras(bundle);
                 startActivity(intent);
-                // startActivity(new Intent(EditProfileActivity.this, Profile_Activity.class));
-//
-//                // Crea un intent per tornare all'activity precedente
-//                Intent intent = new Intent();
-//                intent.putExtra("name", newName);
-//                intent.putExtra("email", newEmail);
-//                setResult(RESULT_OK, intent);
-//
-//                // Lettura della nuova email dall'EditText
-//                newEmail = editEmail.getText().toString();
 
                 finish();
             }
