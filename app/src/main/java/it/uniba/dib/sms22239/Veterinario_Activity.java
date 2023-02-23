b1086a6d9b58503c19362a9c172cda6785fcb27f
Fede <giuliani.f2001@gmail.com>
2023-02-23 15:35:51 +0100

    riaggiunto veterinario

---
+++ app/src/main/java/it/uniba/dib/sms22239/Veterinario_Activity.java
@@ -0,0 +1,89 @@
+package it.uniba.dib.sms22239;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+
+public class Veterinario_Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veterinario);
+        Load_setting();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new Fragment_profile_veterinario());
+        fragmentTransaction.commit();
+
+        findViewById(R.id.home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Veterinario_Activity.this, HomeActivity.class));
+            }
+        });
+
+        findViewById(R.id.profile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Veterinario_Activity.this, Veterinario_Activity.class));
+            }
+        });
+
+        findViewById(R.id.annunci).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        findViewById(R.id.pet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        findViewById(R.id.qr).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        findViewById(R.id.impostazioni).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Veterinario_Activity.this, Preference.class));
+            }
+        });
+
+    }
+
+    private void Load_setting()
+    {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String orien = sp.getString("ORIENTATION", "false");
+        if ("1".equals(orien)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_BEHIND);
+        } else if ("2".equals(orien)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else if ("3".equals(orien)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        Load_setting();
+        super.onResume();
+    }
+}
