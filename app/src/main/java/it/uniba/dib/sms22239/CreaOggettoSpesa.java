3a994815f53a4b780fee0a6b87c2860fe5de28be
To527nio <antonioiungo01@gmail.com>
2023-02-24 16:33:13 +0100

    Modifiche delle classi e layout inerenti alla gestione delle spese

--- app/src/main/java/it/uniba/dib/sms22239/CreaOggettoSpesa.java
+++ app/src/main/java/it/uniba/dib/sms22239/CreaOggettoSpesa.java
@@ -3,11 +3,18 @@ package it.uniba.dib.sms22239;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
 public class CreaOggettoSpesa extends AppCompatActivity {
+
+    EditText inputData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +60,28 @@ public class CreaOggettoSpesa extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CreaOggettoSpesa.this, Preference.class));
+            }
+        });
+
+        findViewById(R.id.add_item_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String datatmp = inputData.getText().toString();
+                String data = "";
+                CharacterIterator it = new StringCharacterIterator(datatmp);
+
+                while (it.current() != CharacterIterator.DONE)
+                {
+                    if(it.getIndex() == 4 || it.getIndex() == 6 )
+                    {
+                        data = data + "/";
+                    }
+                    data = data + it.current();
+                    it.next();
+                }
+
+                startActivity(new Intent(CreaOggettoSpesa.this, Spese.class));
             }
         });
 
