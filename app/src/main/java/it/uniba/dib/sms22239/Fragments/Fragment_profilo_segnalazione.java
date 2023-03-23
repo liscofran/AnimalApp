6645dbbd558a3d623a904af46190c77f8bebc3c8
Fede <giuliani.f2001@gmail.com>
2023-03-23 16:24:57 +0100

    modificato il Fragment_profilo_segnalazione , aggiunto il codice per caricare il layout a seconda della segnalazione selezionata (non funziona ancora)

--- app/src/main/java/it/uniba/dib/sms22239/Fragments/Fragment_profilo_segnalazione.java
+++ app/src/main/java/it/uniba/dib/sms22239/Fragments/Fragment_profilo_segnalazione.java
@@ -92,6 +92,7 @@ public class Fragment_profilo_segnalazione extends Fragment
 
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
