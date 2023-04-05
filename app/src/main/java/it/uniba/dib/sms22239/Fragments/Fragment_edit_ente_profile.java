467645b0a60b926f44f9eb428a59c8a3fe55f67e
Fede <giuliani.f2001@gmail.com>
2023-04-05 17:13:51 +0200

    aggiunto lo spinner in ente

--- app/src/main/java/it/uniba/dib/sms22239/Fragments/Fragment_edit_ente_profile.java
+++ app/src/main/java/it/uniba/dib/sms22239/Fragments/Fragment_edit_ente_profile.java
@@ -11,8 +11,11 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,10 +33,14 @@ public class Fragment_edit_ente_profile extends Fragment {
 
     private FirebaseAuth mAuth;
     private TextView mragionesociale;
-    private TextView mtipoTextView;
+    //private TextView mtipoTextView;
 
     private TextView mpivaTextView;
     private TextView msedelegaleTextView;
+
+    private Spinner spinner;
+
+    String selectedItem;
 
     public Fragment_edit_ente_profile() {
         // Required empty public constructor
@@ -43,18 +50,21 @@ public class Fragment_edit_ente_profile extends Fragment {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.fragment_edit_profile_ente, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         super.onViewCreated(view, savedInstanceState);
+        spinner = getView().findViewById(R.id.spinner);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
@@ -64,15 +74,35 @@ public class Fragment_edit_ente_profile extends Fragment {
 
         // Collega i componenti dell'interfaccia con le variabili
         EditText editragsociale = getView().findViewById(R.id.ente_ragione_sociale);
-        EditText edittipo = getView().findViewById(R.id.ente_tipo);
         EditText editsedelegale = getView().findViewById(R.id.ente_sede_legale);
         EditText editpiva = getView().findViewById(R.id.ente_piva);
+
+
+        spinner.setPrompt("");
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.ente_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedItem = (String) parent.getItemAtPosition(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
 
 
         Button saveProfileButton = getView().findViewById(R.id.save_profile_button);
         mragionesociale = getView().findViewById(R.id.ente_ragione_sociale);
-        mtipoTextView = getView().findViewById(R.id.ente_tipo);
+//        mtipoTextView = getView().findViewById(R.id.ente_tipo);
         msedelegaleTextView = getView().findViewById(R.id.ente_sede_legale);
         mpivaTextView = getView().findViewById(R.id.ente_piva);
 
@@ -84,13 +114,11 @@ public class Fragment_edit_ente_profile extends Fragment {
             {
                 //recupero dati e assegnazione alle variabili
                 String ragsociale = dataSnapshot.child("ragione_sociale").getValue(String.class);
-                String tipo = dataSnapshot.child("tipo").getValue(String.class);
                 String sedelegale = dataSnapshot.child("sede_legale").getValue(String.class);
                 String piva = dataSnapshot.child("p_iva").getValue(String.class);
 
                 //set delle variabili recuperate al layout
                 mragionesociale.setText(ragsociale);
-                mtipoTextView.setText(tipo);
                 msedelegaleTextView.setText(sedelegale);
                 mpivaTextView.setText(piva);
             }
@@ -114,7 +142,7 @@ public class Fragment_edit_ente_profile extends Fragment {
 
                 //prende i dati inseriti in input e gli assegna alle variabili temporanee
                 String newRagsociale = editragsociale.getText().toString();
-                String newtipo = edittipo.getText().toString();
+                String newtipo = selectedItem;
                 String newsedelegale = editsedelegale.getText().toString();
                 String newpiva = editpiva.getText().toString();
 
