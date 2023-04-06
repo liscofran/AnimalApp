322d65c41e52b4a3fe93aee29b92251da17750de
To527nio <antonioiungo01@gmail.com>
2023-04-06 12:09:39 +0200

    Aggiunta Appuntamenti del veterinario sul database

--- app/src/main/java/it/uniba/dib/sms22239/Activities/Activity_Nuovo_Appuntamento.java
+++ app/src/main/java/it/uniba/dib/sms22239/Activities/Activity_Nuovo_Appuntamento.java
@@ -1,5 +1,6 @@
 package it.uniba.dib.sms22239.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.common.net.InternetDomainName;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.time.LocalDate;
@@ -27,8 +36,8 @@ public class Activity_Nuovo_Appuntamento extends AppCompatActivity {
 
     private CalendarView calendarView;
     private Button salvaButton;
-    private int idVeterinario;
-    private int idAppuntamento;
+    String idVeterinario;
+    private String idAppuntamento;
     private FirebaseFirestore db;
 
     @Override
@@ -39,46 +48,64 @@ public class Activity_Nuovo_Appuntamento extends AppCompatActivity {
         // Inizializza gli elementi della UI
         calendarView = findViewById(R.id.simpleCalendarView);
         salvaButton = findViewById(R.id.salva);
-        db = FirebaseFirestore.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+
+
 
         // Imposta il listener sul CalendarView per ottenere la data selezionata dall'utente
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
                 // Creazione dell'oggetto Appuntamento con i dati selezionati dall'utente
-                TimePicker timePicker = findViewById(R.id.ora_inizio);
 
-
-
-                int ora_inizio = timePicker.getHour();
-                int minuti_inizio = timePicker.getMinute();
-                Calendar inizio = Calendar.getInstance();
-                inizio.set(Calendar.HOUR_OF_DAY, ora_inizio);
-                inizio.set(Calendar.MINUTE, minuti_inizio);
-
-                TimePicker timePicker2 = findViewById(R.id.ora_fine);
-
-                int ora_fine = timePicker.getHour();
-                int minuti_fine = timePicker.getMinute();
-                Calendar fine = Calendar.getInstance();
-                fine.set(Calendar.HOUR_OF_DAY, ora_inizio);
-                fine.set(Calendar.MINUTE, minuti_inizio);
 
                 String data = LocalDate.of(year, month + 1, day).format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-                Appuntamento appuntamento = new Appuntamento(idVeterinario, inizio, fine, data, idAppuntamento);
 
 
-                // Imposta il listener sul bottone "Salva"
-                salvaButton.setOnClickListener(new View.OnClickListener() {
+                DatabaseReference mDatabase1 = database.getInstance().getReference().child("User").child(user.getUid());
+
+                // Aggiungi un listener per ottenere i dati del veterinario
+                mDatabase1.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
-                    public void onClick(View view) {
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        // Recupera l'ID del veterinario dal DataSnapshot
+                        idVeterinario = dataSnapshot.getKey();
+
+
+                        // Imposta il listener sul bottone "Salva"
+                        salvaButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                TimePicker timePicker = findViewById(R.id.ora_inizio);
+                                LocalTime ora_inizio = LocalTime.of(timePicker.getHour(), timePicker.getMinute());
+
+                                TimePicker timePicker2 = findViewById(R.id.ora_fine);
+                                LocalTime ora_fine = LocalTime.of(timePicker2.getHour(), timePicker2.getMinute());
+
+                                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+                                String ora_inizio_string = ora_inizio.format(formatter);
+                                String ora_fine_string = ora_fine.format(formatter);
+
+
+                                Appuntamento appuntamento = new Appuntamento(idVeterinario, ora_inizio_string, ora_fine_string, data, idAppuntamento);
+                                appuntamento.writeNewAppuntamento(idVeterinario, ora_inizio_string, ora_fine_string, data, appuntamento);
+                                Intent intent = new Intent(Activity_Nuovo_Appuntamento.this, Activity_Prenotazioni_Veterinario.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
                     }
                 });
 
             }
         });
-
     }
 }
-
--- app/src/main/java/it/uniba/dib/sms22239/Models/Appuntamento.java
+++ app/src/main/java/it/uniba/dib/sms22239/Models/Appuntamento.java
@@ -1,26 +1,87 @@
 package it.uniba.dib.sms22239.Models;
 
-import java.util.Calendar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.PropertyName;
 
 public class Appuntamento {
-     int id_veterinario;
-     Calendar orario_inizio;
-     Calendar orario_fine;
-     String data;
-     int id_appuntamento;
 
-    public Appuntamento(int id_veterinario, Calendar inizio, Calendar fine, String data, int id_appuntamento) {
+    @PropertyName("id_veterinario")
+    private String id_veterinario;
+
+    @PropertyName("orario_inizio")
+    private String orario_inizio;
+
+    @PropertyName("orario_fine")
+    private String orario_fine;
+
+    @PropertyName("data")
+    private String data;
+
+    @PropertyName("id_appuntamento")
+    private String id_appuntamento;
+
+    public Appuntamento() {
+    }
+
+    public Appuntamento(String id_veterinario, String inizio, String fine, String data, String id_appuntamento) {
         this.id_veterinario = id_veterinario;
-        this.orario_inizio = inizio;
-        this.orario_fine = fine;
+        this.orario_inizio = String.valueOf(inizio);
+        this.orario_fine = String.valueOf(fine);
         this.data = data;
         this.id_appuntamento = id_appuntamento;
     }
 
-    public void writeNewAppuntamento (int id_veterinario, Calendar inizio, Calendar fine, String data, int id_appuntamento, Appuntamento ap){
-
+    public String getId_veterinario() {
+        return id_veterinario;
     }
 
+    public void setId_veterinario(String id_veterinario) {
+        this.id_veterinario = id_veterinario;
+    }
 
+    public String getOrario_inizio() {
+        return orario_inizio;
+    }
+
+    public void setOrario_inizio(String orario_inizio) {
+        this.orario_inizio = orario_inizio;
+    }
+
+    public String getOrario_fine() {
+        return orario_fine;
+    }
+
+    public void setOrario_fine(String orario_fine) {
+        this.orario_fine = orario_fine;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getId_appuntamento() {
+        return id_appuntamento;
+    }
+
+    public void setId_appuntamento(String id_appuntamento) {
+        this.id_appuntamento = id_appuntamento;
+    }
+
+    public void writeNewAppuntamento(String id_veterinario, String inizio, String fine, String data, Appuntamento appuntamento){
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        String key = mDatabase.child("Appuntamenti").push().getKey();
+        this.setId_veterinario(id_veterinario);
+        this.setOrario_inizio(inizio);
+        this.setOrario_fine(fine);
+        this.setData(data);
+        this.setId_appuntamento(key);
+        mDatabase.child("Appuntamenti").child(key).setValue(this);
+    }
 }
 
