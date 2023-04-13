c644eb179f1b2043fb510e17af447874d2cba18a
Gabriele <gabrieleleone812@gmail.com>
2023-04-13 16:22:52 +0200

    test su mappa per Activity_Registrazione_Segnalazione

--- app/src/main/java/it/uniba/dib/sms22239/Activities/Activity_Registrazione_Segnalazione.java
+++ app/src/main/java/it/uniba/dib/sms22239/Activities/Activity_Registrazione_Segnalazione.java
@@ -1,13 +1,20 @@
 package it.uniba.dib.sms22239.Activities;
 
 
+
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.CheckBox;
@@ -17,6 +24,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +36,7 @@ import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
@@ -36,6 +45,7 @@ import androidx.core.content.ContextCompat;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import it.uniba.dib.sms22239.MapsActivity;
 import it.uniba.dib.sms22239.Preference;
 import it.uniba.dib.sms22239.R;
 import it.uniba.dib.sms22239.Models.Segnalazione;
@@ -43,20 +53,28 @@ import it.uniba.dib.sms22239.Models.Segnalazione;
 public class Activity_Registrazione_Segnalazione extends AppCompatActivity {
 
     EditText oggettoText, provinciaText, Descrizione;
-    CheckBox proprietario,ente,veterinario;
+    CheckBox proprietario, ente, veterinario;
+    static final int REQUEST_LOCATION_PERMISSION = 1;
+
     boolean checkProprietario, checkEnte, checkVeterinario;
-    ImageButton allegato,photoBtn,submitBtn,backBtn;
+
+
+    ImageButton allegato, photoBtn, submitBtn;
 
     private static final int PICK_IMAGE_REQUEST = 1;
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
 
     Uri mImageUri;
     ImageView mImageView;
+    private LatLng userLocation;
 
     StorageReference mStorageRef;
     DatabaseReference mDatabaseRef;
 
     StorageTask mUploadTask;
+
+    ImageButton backBtn;
+    ImageButton modificaBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,11 +83,10 @@ public class Activity_Registrazione_Segnalazione extends AppCompatActivity {
 
         allegato = findViewById(R.id.allegatoBtn);
         photoBtn = findViewById(R.id.photoBtn);
+        modificaBtn = findViewById(R.id.modificaBtn);
         submitBtn = findViewById(R.id.submitBtn);
         mImageView = findViewById(R.id.image_view);
-
         mStorageRef = FirebaseStorage.getInstance().getReference("Segnalazioni");
-
         allegato.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,6 +103,23 @@ public class Activity_Registrazione_Segnalazione extends AppCompatActivity {
                 } else {
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(intent, 0);
+                }
+            }
+        });
+        modificaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check if the location permission is already granted
+                if (ContextCompat.checkSelfPermission(Activity_Registrazione_Segnalazione.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is already granted
+                    // Get the user's location
+                    getLocation();
+                } else {
+                    // Permission is not granted yet
+                    // Request the permission from the user
+                    ActivityCompat.requestPermissions(Activity_Registrazione_Segnalazione.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_LOCATION_PERMISSION);
                 }
             }
         });
@@ -142,8 +176,9 @@ public class Activity_Registrazione_Segnalazione extends AppCompatActivity {
             }
         });
 
+
         oggettoText = findViewById(R.id.oggettotext);
-        provinciaText =findViewById(R.id.provinciatext);
+        provinciaText = findViewById(R.id.provinciatext);
         Descrizione = findViewById(R.id.descrizione);
 
         proprietario = findViewById(R.id.check_box1);
@@ -209,7 +244,7 @@ public class Activity_Registrazione_Segnalazione extends AppCompatActivity {
                 } else {
                     uploadFile(sgn);
                 }
-                sgn.writeSegnalazione(sgn, oggetto, provincia, descrizione,checkProprietario,checkEnte,checkVeterinario);
+                sgn.writeSegnalazione(sgn, oggetto, provincia, descrizione, checkProprietario, checkEnte, checkVeterinario);
                 Intent intent = new Intent(Activity_Registrazione_Segnalazione.this, Activity_Segnalazioni_Offerte.class);
                 intent.putExtra("SEGNALAZIONE_CODE", sgn.idSegnalazione);
                 startActivity(intent);
@@ -276,5 +311,102 @@ public class Activity_Registrazione_Segnalazione extends AppCompatActivity {
             Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
         }
         return segnalazione;
+    }
+
+    private void getLocation() {
+        // Create a LocationManager object
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Check if the GPS is enabled
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            // Request the location updates
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    // Handle the new location data
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    userLocation = new LatLng(latitude, longitude);
+                    showMapAlert();                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                }
+            });
+        } else {
+            // GPS is not enabled
+            // Show a dialog to the user to enable the GPS
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Per usare questa modalità devi abilitare il GPS, vuoi abilitarlo ? ")
+                    .setCancelable(false)
+                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // Open the location settings screen
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // Do nothing
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+    private void showMapAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Activity_Registrazione_Segnalazione.this);
+        builder.setMessage("Do you want to view the map?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Launch the map activity
+                        Intent intent = new Intent(Activity_Registrazione_Segnalazione.this, MapsActivity.class);
+                        intent.putExtra("userLocation", userLocation);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                // Get the user's location
+                getLocation();
+            } else {
+                // Permission is denied
+                // Handle the case where the user denies the permission
+            }
+        }
     }
 }
